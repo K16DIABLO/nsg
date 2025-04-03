7a5d548e5a26bf0c2119209f10c3146727140483
k16diablo <miguel92@naver.com>
2025-04-03 15:35:00 +0900

    baseline with recall

--- tests/test_nsg_optimized_search.cpp
+++ tests/test_nsg_optimized_search.cpp
@@ -6,6 +6,8 @@
 #include <efanna2e/util.h>
 #include <chrono>
 #include <string>
+#include <omp.h>
+#include <iomanip>
 
 void load_data(char* filename, float*& data, unsigned& num,
                unsigned& dim) {  // load data with sift10K pattern
@@ -30,6 +32,29 @@ void load_data(char* filename, float*& data, unsigned& num,
   in.close();
 }
 
+// Sungjun Jung: Read groundtruth 
+void load_data_ivecs(char* filename, unsigned int*& data, unsigned& num,
+               unsigned& dim) { 
+  std::ifstream in(filename, std::ios::binary);
+  if (!in.is_open()) {
+    std::cout << "open file error" << std::endl;
+    exit(-1);
+  }
+  in.read((char*)&dim, 4);
+  in.seekg(0, std::ios::end);
+  std::ios::pos_type ss = in.tellg();
+  size_t fsize = (size_t)ss;
+  num = (unsigned)(fsize / (dim + 1) / 4);
+  data = new unsigned int[(size_t)num * (size_t)dim];
+
+  in.seekg(0, std::ios::beg);
+  for (size_t i = 0; i < num; i++) {
+    in.seekg(4, std::ios::cur);
+    in.read((char*)(data + i * dim), dim * 4);
+  }
+  in.close();
+}
+
 void save_result(const char* filename,
                  std::vector<std::vector<unsigned> >& results) {
   std::ofstream out(filename, std::ios::binary | std::ios::out);
@@ -42,9 +67,9 @@ void save_result(const char* filename,
   out.close();
 }
 int main(int argc, char** argv) {
-  if (argc != 7) {
+  if (argc != 9) {
     std::cout << argv[0]
-              << " data_file query_file nsg_path search_L search_K result_path"
+              << " data_file query_file groundtruth_file nsg_path search_L search_K result_path num_threads"
               << std::endl;
     exit(-1);
   }
@@ -56,19 +81,24 @@ int main(int argc, char** argv) {
   load_data(argv[2], query_load, query_num, query_dim);
   assert(dim == query_dim);
 
-  unsigned L = (unsigned)atoi(argv[4]);
-  unsigned K = (unsigned)atoi(argv[5]);
+  unsigned L = (unsigned)atoi(argv[5]);
+  unsigned K = (unsigned)atoi(argv[6]);
 
   if (L < K) {
     std::cout << "search_L cannot be smaller than search_K!" << std::endl;
     exit(-1);
   }
 
+  // Sungjun Jung: Load groundtruth to compute recall
+  uint32_t* ground_truth_load = NULL;
+  uint32_t ground_truth_num, ground_truth_dim;
+  load_data_ivecs(argv[3], ground_truth_load, ground_truth_num, ground_truth_dim);
+
   // data_load = efanna2e::data_align(data_load, points_num, dim);//one must
   // align the data before build query_load = efanna2e::data_align(query_load,
   // query_num, query_dim);
   efanna2e::IndexNSG index(dim, points_num, efanna2e::FAST_L2, nullptr);
-  index.Load(argv[3]);
+  index.Load(argv[4]);
   index.OptimizeGraph(data_load);
 
   efanna2e::Parameters paras;
@@ -78,15 +108,35 @@ int main(int argc, char** argv) {
   std::vector<std::vector<unsigned> > res(query_num);
   for (unsigned i = 0; i < query_num; i++) res[i].resize(K);
 
+  uint32_t num_threads = (uint32_t)atoi(argv[8]);
+  omp_set_num_threads(num_threads);
+
   auto s = std::chrono::high_resolution_clock::now();
+#pragma omp parallel for schedule(dynamic, 1)
   for (unsigned i = 0; i < query_num; i++) {
     index.SearchWithOptGraph(query_load + i * dim, K, paras, res[i].data());
   }
   auto e = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double> diff = e - s;
-  std::cout << "search time: " << diff.count() << "\n";
+//  std::cout << "search time: " << diff.count() << "\n";
 
-  save_result(argv[6], res);
+  save_result(argv[7], res);
+
+  // Sungjun Jung: Evaluate Recall
+  unsigned int topk_hit = 0;
+  for (unsigned int i = 0; i < query_num; i++) {
+    for (unsigned int j = 0; j < K; j++) {
+      for (unsigned int k = 0; k < K; k++) {
+        if (res[i][j] == *(ground_truth_load + i * ground_truth_dim + k)) {
+          topk_hit++;
+          break;
+        }
+      }
+    }
+  }
+  float recall = (float)topk_hit / (query_num * K) * 100;
+
+  std::cout << std::left << std::setw(5) << L << std::setw(10) << query_num / diff.count() << std::setw(10) << recall << std::endl;
 
   return 0;
 }
